b4a8af7ed95f327d1cf1730c96ce0c899b6377c8
Mares <66750744+mareszhar@users.noreply.github.com>
2024-12-05 11:52:32 -0400

    feat: replicate dorilama's nuxt sandbox with nuxt 4 folder structure

---
+++ app/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="appId">
+      <h1>hello</h1>
+      <p>Connection status: {{ connectionStatus }}</p>
+      <Todo />
+      <ClientOnly>
+        <Todo />
+      </ClientOnly>
+      <ClientOnlyTodo />
+    </div>
+    <p v-else>
+      add your instant appId in .env (INSTANT_APP_ID="") or directly in nuxt
+      config
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const appId = useRuntimeConfig().public.instantAppId
+const db = useDb()
+const connectionStatus = db.useConnectionStatus()
+</script>
